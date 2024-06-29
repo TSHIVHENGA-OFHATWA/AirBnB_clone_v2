9575497ff23ae6177017089ba177875839cb3bc6
ofhatwa <ofhatwa@ofhatwa.tdo.com>
2024-06-30 00:51:32 +0200

    Adding base model test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" """
+"""
+Unit tests for the BaseModel class
+"""
+
+
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -9,38 +13,39 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """Test cases for the BaseModel class."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize the test case."""
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """Set up the test case environment."""
         pass
 
     def tearDown(self):
+        """Tear down the test case environment."""
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """ """
+        """Test default instantiation of BaseModel."""
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """Test instantiation with kwargs."""
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """Test instantiation with invalid kwargs."""
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -57,41 +62,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """Test the __str__ method of BaseModel."""
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """Test the to_dict method of BaseModel."""
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """Test instantiation with None kwargs."""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """Test instantiation with a single invalid kwarg."""
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ """
+        """Test the id attribute of BaseModel."""
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """Test the created_at attribute of BaseModel."""
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """Test the updated_at attribute of BaseModel."""
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
