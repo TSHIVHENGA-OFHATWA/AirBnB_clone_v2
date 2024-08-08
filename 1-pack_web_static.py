2a9b56cc6fb5e721e23d59c05408e05dfb73b95f
ofhatwa <tofhatwa@gmail.com>
2024-08-08 22:52:11 +0200

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-Fabric script to generate a .tgz archive from the contents of the web_static folder
+Fabric script to generate a .tgz archive from the contents
+of the web_static folder
 """
 
 from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
-    
+
     Returns:
         str: The archive path if the archive has been correctly generated.
         None: If the archive hasn't been generated.
@@ -21,7 +23,8 @@ def do_pack():
 
     # Generate the archive name
     now = datetime.now()
-    archive_name = "versions/web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
+    archive_name = "versions/web_static_{}.tgz"
+    .format(now.strftime("%Y%m%d%H%M%S"))
 
     # Create the archive
     command = "tar -czvf {} web_static".format(archive_name)
